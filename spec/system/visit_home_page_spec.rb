e24083a169ac017511a5267e11580f65c7ca30e8
Adam Okoń <adam.okon@elegantcode.pl>
2023-10-29 13:41:15 +0100

    add basic UI for home page

---
+++ spec/system/visit_home_page_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Visit home pages", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  context "when search is not used" do
+    it "displays valid marketing content" do
+      visit root_url
+
+      expect(page).to have_text("Discover more with our powerful search. Find what you're looking for quickly and easily.")
+    end
+
+    it "displays faked search results" do
+      visit root_url
+
+      fake_results = all(".test-fake-result")
+
+      expect(fake_results.size).to eq(2)
+    end
+  end
+end
